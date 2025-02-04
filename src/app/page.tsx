76ace2ba8751c17eab2e1039a9531dfc179bbd77
pedro <pedro.h.a.moura.business@gmail.com>
2025-02-03 23:21:59 -0300

    ♻️ | refac(main): change the hero section to the new prototype.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Hero } from "@/components";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start max-w-[90%]">
+    <div className="flex flex-col items-center mt-24 w-100vw">
+      <main className="flex flex-col gap-8 row-start-2 w-full">
         <Hero />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -4,33 +4,45 @@ import Image from "next/image";
 
 export const Hero = () => {
   return (
-    <section className="flex flex-col gap-7 items-center md:flex-row my-5">
-      <div className="flex flex-col text-center gap-7 items-center md:items-start">
-        <header>
-          <h1 className="text-2xl text-accent font-black-mango-regular md:text-3xl">
-            Thamis Moura
-          </h1>
-          <h2>CRN3 66118</h2>
-        </header>
+    <section className="flex flex-col items-center md:flex-row my-5 w-full">
+      <div className="h-[350px] w-full relative z-50">
+        <Image
+          src="/food.jpg"
+          height={144}
+          width={192}
+          alt=""
+          className="w-full h-full z-10 absolute top-0 blur-lg"
+        />
 
-        <h3 className="text-3xl font-prompt text-accent-foreground w-96 md:text-left md:text-4xl">
-          Sua jornada para uma vida mais saudável começa aqui!
-        </h3>
+        <div className="absolute top-0 z-20 h-full">
+          <div className="flex flex-col text-center items-start p-5 h-full justify-center gap-14">
+            {/* <header className="text-primary-foreground">
+              <h1 className="text-2xl font-black-mango-regular md:text-3xl">
+                Thamis Moura
+              </h1>
+              <h2>CRN3 66118</h2>
+            </header> */}
 
-        <Button
-          className="w-40 font-noto-sans md:text-lg md:w-48"
-          variant="accent-foreground"
-          size="lg"
-        >
-          Começar agora
-        </Button>
+            <h3 className="text-4xl font-prompt text-primary-foreground w-96 text-left md:text-4xl">
+              Sua jornada para uma vida mais saudável começa aqui!
+            </h3>
+
+            <Button
+              className="font-noto-sans md:text-lg md:w-48 bg-btn-custom animate-pulse-custom"
+              variant="accent-foreground"
+              size="lg"
+            >
+              Saiba mais
+            </Button>
+          </div>
+        </div>
       </div>
 
       <Image
         src="/site4.webp"
-        height={400}
-        width={400}
-        className="rounded-lg md:h-[290px] md:w-[300px]"
+        height={350}
+        width={350}
+        className="rounded-lg w-full md:h-[290px] md:w-[300px]"
         alt="Imagem de apresentação da Dra. Thamis Moura"
       />
     </section>
