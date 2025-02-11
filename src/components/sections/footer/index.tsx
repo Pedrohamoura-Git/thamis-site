df9abc505000a94410029a6c1625ad6d7335cb83
pedro <pedro.h.a.moura.business@gmail.com>
2025-02-11 00:03:17 -0300

    📱 | responsive(main): add responsiveness in the footer section for medium and large screen devices.

--- src/components/sections/footer/index.tsx
+++ src/components/sections/footer/index.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <footer className="mt-20 flex w-full flex-col justify-start gap-7 bg-muted p-10 text-sm text-primary-foreground">
-      <div className="flex items-center gap-5">
-        <Logo width={80} height={80} fill="white" />
+    <footer className="mt-20 flex w-full flex-col justify-start gap-7 bg-muted p-10 text-sm text-primary-foreground md:flex-row md:justify-evenly md:gap-10 md:px-2 lg:gap-20 lg:p-14">
+      <div className="flex items-center gap-5 md:columns-1 md:flex-col md:text-center">
+        <Logo width={80} height={80} fill="white" className="md:h-16 md:w-16" />
 
         <div className="flex flex-col items-center">
-          <h5 className="font-black-mango-regular text-2xl md:text-3xl">
+          <h5 className="font-black-mango-regular text-2xl md:text-xl">
             Dra. Thamis Moura
           </h5>
           <h6 className="text-xs uppercase">nutricionista</h6>
@@ -38,29 +38,27 @@ export const Footer = () => {
 
         <p>Segunda a sexta: 08h-18h</p>
         <p>Sábado: 09h-12h</p>
-      </div>
 
-      <div>
-        <h5 className="mb-2 text-base font-bold text-foreground">
+        <h5 className="mb-2 mt-4 text-base font-bold text-foreground">
           Atendimento online
         </h5>
 
-        <p>Em todo lugar do Brasil</p>
+        <p>Em todo Brasil</p>
       </div>
 
       <div>
         <h5 className="mb-2 text-base font-bold text-foreground">Contato</h5>
 
         <p>thamismoura@gmail.com</p>
-      </div>
 
-      <Button
-        className="bg-btn-custom font-noto-sans animate-pulse-custom w-48 md:text-lg"
-        variant="accent-foreground"
-        size="lg"
-      >
-        Agendar sua consulta
-      </Button>
+        <Button
+          className="bg-btn-custom font-noto-sans animate-pulse-custom mt-4 w-48"
+          variant="accent-foreground"
+          size="lg"
+        >
+          Agendar sua consulta
+        </Button>
+      </div>
     </footer>
   );
 };
