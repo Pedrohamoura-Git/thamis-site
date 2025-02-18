25b8c23a664195ee55f0afdd6dbbb3681fcaf91e
pedro <pedro.h.a.moura.business@gmail.com>
2025-02-18 00:30:17 -0300

    📱 | responsive(main): add dynamic imageSrc values for different screen sizes.

--- src/components/sections/hero/index.tsx
+++ src/components/sections/hero/index.tsx
@@ -1,40 +1,46 @@
+"use client";
+
 import Image from "next/image";
-import { Button } from "@/components/ui";
+import { useEffect, useState } from "react";
+import { Arrow } from "@/widgets";
 
 export const Hero = () => {
+  const [imageSrc, setImageSrc] = useState("/home-sm.jpg");
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setImageSrc("/home-sm.jpg");
+      } else if (window.innerWidth <= 1024) {
+        setImageSrc("/basil-herbs-green-mediterranean-40720.jpg");
+      } else {
+        setImageSrc("/home-2.jpg");
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <section className="z-10 mb-5 mt-32 flex w-full flex-col items-center md:max-h-[300px] md:flex-row md:overflow-hidden">
-      <div className="glossy-effect-custom relative z-50 w-full overflow-hidden before:h-full before:w-[200%] md:h-full md:before:h-[200%] md:before:w-full">
+    <section className="z-10 mb-5 flex w-full flex-col items-center md:flex-row md:overflow-hidden">
+      <div className="glossy-effect-custom relative z-50 h-screen w-full overflow-hidden before:h-[200%] before:w-[200%] md:h-full">
         <Image
-          // src="/home.jpg"
-          src="/home-sm.jpg"
-          height={400}
-          width={192}
-          alt=""
-          className="z-10 h-full w-full"
+          src={imageSrc}
+          height={5207}
+          width={3673}
+          alt="Imagem de fundo"
+          className="z-10 h-full w-full object-cover"
         />
 
-        <div className="absolute top-0 z-20 h-full">
-          <div className="flex h-full flex-col items-start gap-14 p-5 text-center">
-            {/* <header className="text-primary-foreground">
-              <h1 className="font-black-mango-regular text-2xl md:text-3xl">
-                Thamis Moura
-              </h1>
-              <h2>CRN3 66118</h2>
-            </header> */}
+        <div className="absolute left-1/2 top-2/3 z-20 -translate-x-1/2">
+          <h1 className="font-prompt w-96 text-center text-4xl text-foreground md:text-4xl">
+            Sua jornada para uma vida mais saudável começa aqui!
+          </h1>
 
-            <h1 className="font-prompt w-96 text-left text-4xl text-foreground md:text-4xl">
-              Sua jornada para uma vida mais saudável começa aqui!
-            </h1>
-
-            <Button
-              className="bg-btn-custom font-noto-sans animate-pulse-custom md:w-48 md:text-lg"
-              variant="accent-foreground"
-              size="lg"
-            >
-              Saiba mais
-            </Button>
-          </div>
+          <Arrow href="#about" />
         </div>
       </div>
     </section>
