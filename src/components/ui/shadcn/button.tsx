e4849a1e1b59e29b599488cecdd6270b9cbbea4a
pedro <pedro.h.a.moura.business@gmail.com>
2025-03-11 21:46:31 -0300

    ✨ | feat(new-model): redirect user to the dietist whatsApp on button click.

--- src/components/ui/shadcn/button.tsx
+++ src/components/ui/shadcn/button.tsx
@@ -1,11 +1,13 @@
+"use client";
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
+import { redirectToWhatsAppChat } from "@/utils/redirectToWhatsAppChat";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-sm font-bold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl text-sm font-bold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
@@ -23,8 +25,9 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-full px-3",
-        lg: "h-12 rounded-full px-7",
+        sm: "h-9 rounded-xl px-3",
+        md: "h-11 rounded-xl px-4",
+        lg: "h-12 rounded-xl px-7",
         icon: "h-10 w-10",
       },
     },
@@ -39,15 +42,30 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  message?: string;
+  whatsappNumber?: string;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      message,
+      whatsappNumber,
+      ...props
+    },
+    ref
+  ) => {
     const Comp = asChild ? Slot : "button";
+
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        onClick={() => redirectToWhatsAppChat({ message, whatsappNumber })}
         {...props}
       />
     );
