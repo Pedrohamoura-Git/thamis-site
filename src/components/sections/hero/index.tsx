65a59df72abb0b8898d25c22fa76bc7edce0d04e
pedro <pedro.h.a.moura.business@gmail.com>
2025-03-11 21:40:03 -0300

    📱 | style(new-model): add parallax effect on hero section.

--- src/components/sections/hero/index.tsx
+++ src/components/sections/hero/index.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { Arrow } from "@/widgets";
+import Link from "next/link";
 
 export const Hero = () => {
   const [imageSrc, setImageSrc] = useState("/home-sm.jpg");
@@ -25,22 +26,20 @@ export const Hero = () => {
   }, []);
 
   return (
-    <section className="z-10 flex w-full flex-col items-center md:flex-row md:overflow-hidden">
-      <div className="glossy-effect-custom relative z-50 h-screen w-full overflow-hidden before:h-[200%] before:w-[200%] md:h-full">
-        <Image
-          src={imageSrc}
-          height={5207}
-          width={3673}
-          alt="Imagem de fundo"
-          className="z-10 h-full w-full object-cover"
-        />
-
-        <div className="absolute left-1/2 top-2/3 z-20 -translate-x-1/2">
-          <h1 className="font-prompt w-96 text-center text-4xl text-foreground md:text-4xl">
+    <section className="parallax z-10 flex min-h-screen w-full flex-col items-center md:flex-row md:overflow-hidden">
+      <div className="parallax glossy-effect-custom relative z-50 h-full w-full overflow-hidden before:h-[200%] before:w-[200%] md:h-full">
+        <div className="glossy-effect-custom absolute left-1/2 top-[65%] z-20 h-full w-full -translate-x-1/2 md:top-[72%] xl:top-[65%]">
+          <h1 className="z-25 m-auto w-96 text-center text-4xl text-foreground md:w-3/4 md:text-5xl lg:text-6xl xl:w-3/5 2xl:w-2/5">
             Sua jornada para uma vida mais saudável começa aqui!
           </h1>
 
-          <Arrow href="#about" />
+          <div className="relative mt-20">
+            <Link href="#about" className="arrow-container">
+              <div className="arrow h-6 w-2"></div>
+              <div className="arrow h-6 w-2"></div>
+              <div className="arrow h-6 w-2"></div>
+            </Link>
+          </div>
         </div>
       </div>
     </section>
