ffb199f74f85cd185d4e7c2dade70867b2dc428a
pedro <pedro.h.a.moura.business@gmail.com>
2025-03-11 21:40:37 -0300

    📱 | style(new-model): improve navbar readability and responsiveness on different screen sizes.

--- src/components/sections/navbar/index.tsx
+++ src/components/sections/navbar/index.tsx
@@ -1,12 +1,14 @@
 "use client";
+import { useOutsideClick } from "@/hooks";
 import { cn } from "@/lib/utils";
 import { Logo } from "@/widgets";
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean | null>(false);
+  const ref = useRef<HTMLDivElement>(null);
   const navList = [
     { title: "Inicio", href: "#home" },
     { title: "Sobre mim", href: "#about" },
@@ -15,7 +17,7 @@ export const Navbar = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 50) {
+      if (window.scrollY > 500) {
         setIsScrolled(true);
       } else {
         setIsScrolled(false);
@@ -30,13 +32,17 @@ export const Navbar = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  useOutsideClick(ref, () => setIsMenuOpen(false));
+
   return (
     <nav
-      className={`fixed w-full z-50 transition-all duration-400 ease-in-out ${
-        isScrolled ? "bg-foreground" : "bg-transparent"
-      }`}
+      className={cn(
+        "fixed w-full z-50 transition-all duration-400 ease-in-out",
+        isMenuOpen ? " bg-muted-light" : "bg-transparent",
+        isScrolled && "bg-foreground"
+      )}
     >
-      <div className="container mx-auto px-6 py-3 md:flex md:items-center md:justify-between md:p-2 lg:px-16">
+      <div className="container mx-auto px-6 py-3 md:flex md:items-center md:justify-between md:px-12 md:py-0">
         <div className="flex items-center justify-between">
           <Link href="#home" className="logo">
             <Logo
@@ -47,7 +53,7 @@ export const Navbar = () => {
                 "transition-all ease-linear",
                 isScrolled
                   ? "h-12 w-12 lg:h-16 lg:w-16"
-                  : "h-20 w-20 md:h-24 lg:h-28 md:w-24 lg:w-2h-28"
+                  : "h-16 w-16 md:h-20 lg:h-24 md:w-20 lg:w-24"
               )}
             />
           </Link>
@@ -58,7 +64,7 @@ export const Navbar = () => {
           >
             <i
               className={cn(
-                `block w-8 h-1 rounded mb-1 transition-transform`,
+                "block w-8 h-1 rounded mb-1 transition-transform",
                 isMenuOpen ? "transform rotate-45 translate-y-2" : "",
                 isScrolled ? "bg-primary" : "bg-background"
               )}
@@ -81,18 +87,22 @@ export const Navbar = () => {
         </div>
 
         <div
-          className={cn(`main_list md:block`, isMenuOpen ? "block" : "hidden")}
+          className={cn(`relative md:block`, isMenuOpen ? "block" : "hidden")}
+          ref={ref}
         >
-          <ul className="mt-10 flex h-screen flex-col items-end md:mt-0 md:h-auto md:flex-row md:space-x-8">
+          <ul
+            className={cn(
+              "nav-links mt-10 flex flex-col pb-5 items-end md:mt-0 md:flex-row md:space-x-8 md:p-0",
+              isMenuOpen ? "open" : ""
+            )}
+          >
             {navList.map(({ title, href }, index) => (
               <li className="py-4" key={title + "-" + index}>
                 <Link
                   href={href}
                   className={cn(
-                    `text-xl md:text-lg lg:text-xl`,
-                    isScrolled
-                      ? "text-primary hover:text-green-400"
-                      : "text-foreground"
+                    `text-xl md:text-lg lg:text-xl hover:text-accent-variant`,
+                    isScrolled ? "text-primary" : "text-foreground"
                   )}
                 >
                   {title}
