760f945c8d9371421ddd741a8bd068a2f85c75c5
pedro <pedro.h.a.moura.business@gmail.com>
2025-03-11 21:39:48 -0300

    📱 | style(new-model): transform the cta section into a card on large screen devices for better style.

--- src/components/sections/cta/index.tsx
+++ src/components/sections/cta/index.tsx
@@ -4,31 +4,33 @@ import { CalendarCheck2 } from "lucide-react";
 
 export const Cta = ({ className }: { className?: String }) => {
   return (
-    <div
+    <section
       id="cta"
       className={cn(
-        "mx-auto w-screen bg-card px-8 py-12 md:flex md:items-center md:justify-around md:p-12 lg:max-w-screen-lg",
+        "w-full bg-card px-8 py-12 xl:max-w-screen-lg xl:rounded-2xl",
         className
       )}
     >
-      <div className="md:max-w-sm lg:max-w-xl">
-        <h3 className="text-3xl/10 font-bold lg:text-4xl/normal">
-          Você merece se sentir bem com você mesmo e ter autonomia alimentar.
-        </h3>
+      <div className="mx-auto w-full md:flex md:items-center md:justify-around md:p-12">
+        <div className="md:max-w-sm lg:max-w-xl">
+          <h3 className="text-3xl/10 font-bold lg:text-4xl/normal">
+            Você merece se sentir bem com você mesmo e ter autonomia alimentar.
+          </h3>
 
-        <p className="my-5 md:mt-7 md:text-xl">
-          Atendimento online em todo Brasil
-        </p>
+          <p className="my-5 md:mt-7 md:text-xl">
+            Atendimento online em todo Brasil
+          </p>
+        </div>
+
+        <Button
+          className="bg-btn-custom animate-pulse-custom mt-4 w-48 lg:p-6"
+          variant="accent-foreground"
+          size="lg"
+        >
+          <CalendarCheck2 size={64} />
+          <span className="ml-1">Agendar consulta</span>
+        </Button>
       </div>
-
-      <Button
-        className="bg-btn-custom animate-pulse-custom mt-4 w-48 lg:p-6"
-        variant="accent-foreground"
-        size="lg"
-      >
-        <CalendarCheck2 size={64} />
-        <span className="ml-1">Agendar consulta</span>
-      </Button>
-    </div>
+    </section>
   );
 };
